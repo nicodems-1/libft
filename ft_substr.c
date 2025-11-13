811aee41587e9d2c1699391660dcef69af41d0d6
Nicodeme Verdier <niverdie@z1r11p5.42lyon.fr>
2025-11-13 11:45:29 +0100

    substr

---
+++ ft_substr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: niverdie <niverdie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 18:10:00 by niverdie          #+#    #+#             */
+/*   Updated: 2025/11/13 11:44:59 by niverdie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    char *sub;
+    int i;
+
+    i = 0;
+    sub = malloc(sizeof(char)* (len + 1));
+        if (!sub)
+        {
+            free(sub);
+            return(NULL);
+        }
+    while (len != i && s[start])
+    {
+        sub[i] = s[start];
+        start++;
+        i++;
+    }
+    return(sub);
+}
+/*
+#include <string.h>
+#include <stdio.h>
+
+int main(int ac, char **av)
+{
+    printf("%s", ft_substr(av[1], atoi(av[2]), atoi(av[3])));
+}*/
