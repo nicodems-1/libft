f005c0a55e9623a234f6a916ac6715b451dd4ea4
Nicodeme Verdier <niverdie@z2r6p1.42lyon.fr>
2025-11-17 18:46:06 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,22 +6,25 @@
 /*   By: niverdie <niverdie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 17:04:26 by niverdie          #+#    #+#             */
-/*   Updated: 2025/11/14 11:44:13 by niverdie         ###   ########.fr       */
+/*   Updated: 2025/11/17 18:45:30 by niverdie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include "libft.h"
+#include <stdlib.h>
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*ptr;
+	size_t			i;
 
-	ptr = NULL;
+	i = 0;
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	return (ft_bzero(ptr, nmemb));
+	while (i < nmemb * size)
+		ptr[i++] = 0;
+	return (ptr);
 }
 
 // #include <stdio.h>
