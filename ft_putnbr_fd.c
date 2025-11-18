59b62c9c82a5dbce2d842eb20f0842f0f43b9fca
Nicodeme Verdier <niverdie@z2r5p2.42lyon.fr>
2025-11-18 15:31:45 +0100

    finishing libft mandatory beside split

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,8 +6,34 @@
 /*   By: niverdie <niverdie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 14:43:09 by niverdie          #+#    #+#             */
-/*   Updated: 2025/11/18 14:43:25 by niverdie         ###   ########.fr       */
+/*   Updated: 2025/11/18 15:31:28 by niverdie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void ft_putnbr_fd(int n, int fd);
+#include <unistd.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	char	c;
+
+	if (n == -2147483648)
+	{
+		write(fd, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		n = -n;
+		write(fd, "-", 1);
+	}
+	if (n >= 10)
+		ft_putnbr_fd(n / 10, fd);
+	c = n % 10 + 48;
+	write(fd, &c, 1);
+}
+
+
+// int main(int ac, char **av)
+// {
+//     ft_putnbr_fd(atoi(av[1]), 1);
+// }
